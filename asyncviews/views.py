51cc539827aa281a230c72fe6409b29d20d3b84e
caiodiasol <caiodiasol@proton.me>
2025-10-02 18:55:49 -0300

    ajuste no time

--- asyncviews/views.py
+++ asyncviews/views.py
@@ -5,7 +5,7 @@ async def contador_async(request):
     
     contador = []
     for num in range(1, 6):  
-        await asyncio.sleep(2)  
+        await asyncio.sleep(1.5)  
         contador.append(num)
         print(f"Contador: {num}")
     
